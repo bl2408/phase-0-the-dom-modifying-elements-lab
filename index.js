7b84b352f8a50b2e25fff3a1d927e24918474a5a
Brian Lambert <brianlambert2408@gmail.com>
2022-09-07 09:17:13 +1000

    Completed tasks

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+
+const myName = "Brian";
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = `${myName} is the champion`;
+document.body.append(newHeader);
